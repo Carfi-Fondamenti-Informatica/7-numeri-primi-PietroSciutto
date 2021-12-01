6725d2e59cd604ec40c4e9a93370386397ac1f89
PietroSciutto <91790426+PietroSciutto@users.noreply.github.com>
2021-12-01 22:09:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "lib.h"
 
+bool numeroprimo(int n, int a){
+    if(n<0){
+        return false;
+    }else if(n==1){
+        return true;
+    }else if (a==1){
+        return true;
+    }else if(n%a==0){
+        return false;
+    }
+    numeroprimo(n, --a);
+}
