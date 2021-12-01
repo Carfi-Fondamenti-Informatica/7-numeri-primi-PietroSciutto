d9478928566c0e78e555d697bd3bb688fcb6649a
PietroSciutto <91790426+PietroSciutto@users.noreply.github.com>
2021-12-01 22:10:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
 int main(){
+  int n=0;
+    cin>>n;
+    if(numeroprimo(n, n-1)){
+        cout<<"numero primo";
+    }else{
+        cout<<"numero non primo";
+    }
   return 0;
 }
